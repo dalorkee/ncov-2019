3bcebc27d5eef80ca31967adb4d9ee4840e2ad34
sataphat denduang <sataphat.dev@gmail.com>
2020-02-04 10:58:35 +0700

    ref global country

---
+++ app/GlobalCountry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GlobalCountry extends Model
+{
+	protected $table = 'ref_global_country';
+	protected $primaryKey = 'country_id';
+}
