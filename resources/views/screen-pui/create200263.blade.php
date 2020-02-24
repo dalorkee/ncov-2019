8b2ba0052a9ed84a21055df671ae83b76afc3bfd
sataphat denduang <sataphat.dev@gmail.com>
2020-02-24 10:30:11 +0700

    rm add btn

--- resources/views/screen-pui/create200263.blade.php
+++ resources/views/screen-pui/create200263.blade.php
@@ -74,7 +74,7 @@ $sat_id = Haruncpi\LaravelIdGenerator\IdGenerator::generate($config);
 						<div class="bd-callout bd-callout-info" style="margin-top:0;position:relative">
 							<div style="position:absolute;top:10px;right:10px;z-index:1">
 								<a type="button" href="{{ route('satList') }}" class="btn btn-secondary"><i class="fas fa-arrow-left"></i> Back To Lists SAT</a>
-								<a type="button" href="{{ route('screenpui.create') }}" class="btn btn-info"><i class="fas fa-user-plus"></i> New patient</a>
+								<!-- <a type="button" href="{{ route('screenpui.create') }}" class="btn btn-info"><i class="fas fa-user-plus"></i> New patient</a> -->
 							</div>
 							<div class="card">
 								<div class="card-body">
