ebb56abd722ff0e72b3b474345dba4cd4bd6102a
dalorkee <dalorkee@gmail.com>
2020-03-03 15:31:28 +0700

    new sat list

--- resources/views/layouts/left-sidebar.blade.php
+++ resources/views/layouts/left-sidebar.blade.php
@@ -7,17 +7,10 @@
 		<!-- Sidebar navigation-->
 		<nav class="sidebar-nav">
 			<ul id="sidebarnav" class="p-t-30">
-				<!--
-				<li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{ route('home') }}" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a></li>
-				<li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{ route('satList') }}" aria-expanded="false"><i class="mdi mdi-folder-multiple-outline"></i><span class="hide-menu"> SAT</span></a></li>
-				<li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{ route('investList.index') }}" aria-expanded="false"><i class="mdi mdi-ambulance"></i><span class="hide-menu"> Invest</span></a></li>
-				<li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{ route('allcasecontacttable') }}" aria-expanded="false"><i class="mdi mdi-ambulance"></i><span class="hide-menu"> Contact</span></a></li>
-				-->
 				<li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fas fa-database"></i><span class="hide-menu">Data </span></a>
 					<ul aria-expanded="false" class="collapse  first-level">
 						<li class="sidebar-item"><a href="{{ route('list-data.sat') }}" class="sidebar-link"><i class="mdi mdi-folder-multiple-outline"></i><span class="hide-menu"> SAT</span></a></li>
 						<li class="sidebar-item"><a href="{{ route('list-data.invest') }}" class="sidebar-link"><i class="mdi mdi-ambulance"></i><span class="hide-menu"> Invest</span></a></li>
-						<li class="sidebar-item"><a href="{{ route('allcasecontacttable') }}" class="sidebar-link"><i class="fas fa-code-branch"></i><span class="hide-menu"> Contact</span></a></li>
 						<li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="fas fa-chart-line"></i><span class="hide-menu"> FollowUp </span></a>
 							<ul aria-expanded="false" class="collapse  first-level">
 								<li class="sidebar-item"><a href="{{ route('contactfollowtable') }}" class="sidebar-link"><i class="mdi mdi-account-multiple"></i><span class="hide-menu"> FollowUp Contact</span></a></li>
