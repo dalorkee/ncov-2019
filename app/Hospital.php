2ade7a6da50bcb4383a78dbe0ad97f4a7456afe5
dalorkee <dalorkee@gmail.com>
2020-03-23 09:08:27 +0700

    new screning

---
+++ app/Hospital.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hospital extends Model
+{
+    //
+    protected $table = 'invest_pt';
+	protected $primaryKey = 'id';
+}
--- routes/web.php
+++ routes/web.php
@@ -148,3 +148,6 @@ Route::get('/clusters/doughnut', 'covidController@clusters')->name('maps.doughnu
 Route::get('/einvest', function(App\Exports\InvestExport $export) {
 		return $export->download('inv.xlsx');
 });*/
+
+
+Route::resource('hospital', 'HospitalController');
