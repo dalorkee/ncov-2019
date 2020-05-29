b4e95db98097b019e824d290dbac7d5ac51549f8
dalorkee <dalorkee@gmail.com>
2020-05-29 16:20:48 +0700

    sat nav

--- resources/views/layouts/left-sidebar.blade.php
+++ resources/views/layouts/left-sidebar.blade.php
@@ -21,13 +21,12 @@ $url_to_ddc_ilab = "https://ddc-ilab.invitrace.app?uid=".$uid."&ts=".$ts."&sig="
 					<ul aria-expanded="false" class="collapse  first-level">
 						<li class="sidebar-item"><a href="{{ $url_to_voravit }}" target="_blank" class="sidebar-link"><i class="mdi mdi-chevron-double-right"></i><span class="hide-menu"> PUI Screen</span></a></li>
 						<li class="sidebar-item"><a href="{{ $url_to_ddc_ilab }}" target="_blank" class="sidebar-link"><i class="mdi mdi-chevron-double-right"></i><span class="hide-menu"> DDC iLab</span></a></li>
-						@role('root')
+						@role('root|ddc')
 						<li class="sidebar-item"><a href="{{ route('list-data.sat') }}" class="sidebar-link"><i class="mdi mdi-chevron-double-right"></i><span class="hide-menu"> SAT</span></a></li>
 						@endrole
 						@role('root|ddc|dpc|pho|hos')
 						<li class="sidebar-item"><a href="{{ route('list-data.invest') }}" class="sidebar-link"><i class="mdi mdi-chevron-double-right"></i><span class="hide-menu"> Invest</span></a></li>
 						{{-- <li class="sidebar-item"><a href="{{ route('list.state_quarantine') }}" class="sidebar-link"><i class="mdi mdi-chevron-double-right"></i><span class="hide-menu"> State Quarantine </span></a></li> --}}
-
 						@endrole
 						<li class="sidebar-item"><a href="{{ route('list-data.contact') }}" class="sidebar-link"><i class="mdi mdi-chevron-double-right"></i><span class="hide-menu"> Contact</span></a></li>
 						{{-- <li class="sidebar-item"><a href="{{ route('allcontacttable') }}" class="sidebar-link"><i class="mdi mdi-chevron-double-right"></i><span class="hide-menu"> Contact </span></a> --}}
