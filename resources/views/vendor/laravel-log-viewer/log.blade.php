7e9d9230b48e529e9e116343d9ef90f56e5d972d
dalorkee <dalorkee@gmail.com>
2020-12-12 11:47:19 +0700

    logs viewer

--- resources/views/vendor/laravel-log-viewer/log.blade.php
+++ resources/views/vendor/laravel-log-viewer/log.blade.php
@@ -1,73 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="robots" content="noindex, nofollow">
-  <title>Covid  log viewer</title>
-  <link rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-        crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
-  <style>
-    body {
-      padding: 25px;
-    }
-
-    h1 {
-      font-size: 1.5em;
-      margin-top: 0;
-    }
-
-    #table-log {
-        font-size: 0.85rem;
-    }
-
-    .sidebar {
-        font-size: 0.85rem;
-        line-height: 1;
-    }
-
-    .btn {
-        font-size: 0.7rem;
-    }
-
-    .stack {
-      font-size: 0.85em;
-    }
-
-    .date {
-      min-width: 75px;
-    }
-
-    .text {
-      word-break: break-all;
-    }
-
-    a.llv-active {
-      z-index: 2;
-      background-color: #f5f5f5;
-      border-color: #777;
-    }
-
-    .list-group-item {
-      word-wrap: break-word;
-    }
-
-    .folder {
-      padding-top: 15px;
-    }
-
-    .div-scroll {
-      height: 80vh;
-      overflow: hidden auto;
-    }
-    .nowrap {
-      white-space: nowrap;
-    }
-
-  </style>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="robots" content="noindex, nofollow">
+<title>Covid  log viewer</title>
+<link rel="stylesheet" href="{{ URL::asset('assets/libs/bootstrap-4.3.1/css/bootstrap.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/lisbs/datatables-1.10.20/DataTables-1.10.20/css/dataTables.bootstrap4.min.css') }}">
+<style>
+body {
+ 	padding: 25px;
+}
+h1 {
+	font-size: 1.5em;
+	margin-top: 0;
+}
+#table-log {
+	font-size: 0.85rem;
+}
+.sidebar {
+	font-size: 0.85rem;
+	line-height: 1;
+}
+.btn {
+	font-size: 0.7rem;
+}
+.stack {
+	font-size: 0.85em;
+}
+.date {
+	min-width: 75px;
+}
+.text {
+	word-break: break-all;
+}
+a.llv-active {
+	z-index: 2;
+	background-color: #f5f5f5;
+	border-color: #777;
+}
+.list-group-item {
+	word-wrap: break-word;
+}
+.folder {
+	padding-top: 15px;
+}
+.div-scroll {
+	height: 80vh;
+	overflow: hidden auto;
+}
+.nowrap {
+	white-space: nowrap;
+}
+</style>
 </head>
 <body>
 <div class="container-fluid">
@@ -180,17 +165,13 @@
   </div>
 </div>
 <!-- jQuery for Bootstrap -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+<script src="{{ URL::asset('assets/libs/jquery/dist/jquery.slim.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/bootstrap-4.3.1/js/bootstrap.min.js') }}"></script>
 <!-- FontAwesome -->
-<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+<script defer src="{{ URL::asset('fonts/fontawesome-free-5.13.0-web/js/all.js') }}"></script>
 <!-- Datatables -->
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/libs/datatables-1.10.20/DataTables-1.10.20/js/jquery.dataTables.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/libs/datatables-1.10.20/DataTables-1.10.20/js/dataTables.bootstrap4.min.js') }}"></script>
 <script>
   $(document).ready(function () {
     $('.table-container tr').on('click', function () {
