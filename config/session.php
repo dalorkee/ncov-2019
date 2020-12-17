78e76daa84ab7241d1363e5de67edf25105461cd
dalorkee <dalorkee@gmail.com>
2020-12-17 11:34:18 +0700

    cf

--- config/session.php
+++ config/session.php
@@ -18,8 +18,8 @@ return [
     |
     */
 
-    //'driver' => env('SESSION_DRIVER', 'file'),
-	'driver' => env('SESSION_DRIVER', 'database'),
+    'driver' => env('SESSION_DRIVER', 'file'),
+	//'driver' => env('SESSION_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------
@@ -154,8 +154,8 @@ return [
     |
     */
 
-    //'domain' => env('SESSION_DOMAIN', null),
-	'domain' => env('SESSION_DOMAIN', 'http://203.157.41.155'),
+    'domain' => env('SESSION_DOMAIN', null),
+	//'domain' => env('SESSION_DOMAIN', 'http://192.168.100.115'),
 
     /*
     |--------------------------------------------------------------------------
