e58ad94fe6af1f202989d7c948b6ff403e51995d
dalorkee <dalorkee@gmail.com>
2021-01-08 16:32:33 +0700

    user edit

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -7,8 +7,24 @@
 			<div class="ml-auto text-right">
 				<nav aria-label="breadcrumb">
 					<ol class="breadcrumb">
-						<li class="breadcrumb-item"><a href="{{ route('users.index') }}">User</a></li>
+						<li class="breadcrumb-item"><a href="{{ route('users.index') }}">Users</a></li>
 						<li class="breadcrumb-item active" aria-current="page">Edit</li>
+					</ol>
+				</nav>
+			</div>
+		</div>
+	</div>
+</div>
+@section('contents')
+<div class="page-breadcrumb bg-light">
+	<div class="row">
+		<div class="col-12 d-flex no-block align-items-center">
+			<h4 class="page-title"><span style="display:none;">Create user</span></h4>
+			<div class="ml-auto text-right">
+				<nav aria-label="breadcrumb">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item"><a href="{{ route('users.index') }}">User</a></li>
+						<li class="breadcrumb-item active" aria-current="page">Create</li>
 					</ol>
 				</nav>
 			</div>
@@ -22,8 +38,8 @@
 				<div class="card-body">
 					<div class="d-md-flex align-items-center" style="border-bottom:1px solid #EAEAEA">
 						<div>
-							<h4 class="card-title">บริหารจัดการผู้ใช้งานระบบ</h4>
-							<h5 class="card-subtitle">ID Flu-BOE</h5>
+							<h4 class="card-title">เพิ่มผู้ใช้ใหม่</h4>
+							<h5 class="card-subtitle">DDC Covid-19</h5>
 						</div>
 					</div>
 					<div class="my-4">
@@ -37,57 +53,155 @@
 								</ul>
 							</div>
 						@endif
-						{!! Form::model($user, ['method' => 'PATCH','route' => ['users.update', $user->id]]) !!}
-						<div class="row">
-							<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-								<div class="form-group">
-									<label for"name">คำนำหน้าชื่อ</label>
-									{!! Form::text('title_name', null, array('placeholder' => 'Title name','class' => 'form-control')) !!}
+						{!! Form::open(array('route'=>'users.store', 'method'=>'POST', 'class'=>'mt-4 mb-3')) !!}
+							<div class="row">
+								<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label for="title_name">คำนำหน้าชื่อ<span>*</span></label>
+										<select name="title_name" id="title_name" class="form-control selectpicker show-tick">
+											<option value="">-- โปรดเลือก --</option>
+											<option value="1">นาย</option>
+											<option value="2">นาง</option>
+											<option value="3">นางสาว</option>
+											<option value="4">ด.ช.</option>
+											<option value="5">ด.ญ.</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label for="first_name">ชื่อจริง:<span>*</span></label>
+										<input type="text" name="name" class="form-control" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label for="last_name">นามสกุล:<span>*</span></label>
+										<input type="text" name="lname" class="form-control" placeholder="Lastname">
+									</div>
 								</div>
 							</div>
-							<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-								<div class="form-group">
-									<label for"name">ชื่อ</label>
-									{!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+							<div class="row">
+								<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label for="email">อีเมล์:<span>*</span></label>
+										<input type="email" name="email" class="form-control" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label for="mobile">โทรศัพท์:<span>*</span></label>
+										<input type="text" name="tel" id="tel" class="form-control" placeholder="Mobile">
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label for="idcard">เลขบัตร ปชช:<span>*</span></label>
+										<input type="text" name="card_id" id="card_id" class="form-control" placeholder="ID Card">
+									</div>
 								</div>
 							</div>
-							<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-								<div class="form-group">
-									<label for"lastname">นามสกุล</label>
-									{!! Form::text('lname', null, array('placeholder' => 'Lastname','class' => 'form-control')) !!}
+							<div class="row">
+								<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label for="province">จังหวัด<span>*</span></label>
+										<select name="prov_code" id="prov_code" class="form-control selectpicker show-tick" data-live-search="true">
+											<option value="">-- เลือกจังหวัด --</option>
+												@foreach ($provinces as $key => $value)
+													<option value="{{ $key }}">{{ $value }}</option>
+												@endforeach
+										</select>
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label for="district">อำเภอ:</label>
+										<select name="ampur_code" id="ampur_code" class="form-control selectpicker show-tick" data-live-search="true">
+											<option value="">-- เลือกอำเภอ --</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label for="sub_district">ตำบล:</label>
+										<select name="tambol_code" id="tambol_code" class="form-control selectpicker show-tick" data-live-search="true">
+											<option value="">-- เลือกตำบล --</option>
+										</select>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="row">
-							<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-								<div class="form-group">
-									<label for="email">Email:</label>
-									{!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
+							<div class="row">
+								<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+									<div class="form-group">
+										<label for="hospcode">หน่วยงาน:<span>*</span></label>
+										<select name="hospcode" id="hospcode" class="form-control selectpicker show-tick" data-live-search="true">
+											<option value="">-- เลือกหน่วยงาน --</option>
+										</select>
+									</div>
 								</div>
 							</div>
-							<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-								<div class="form-group">
-									<label for="pwd">Password:</label>
-									{!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control')) !!}
+							<div class="row">
+								<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label>ชื่อผู้ใช้:<span>*</span></label>
+										<input type="text" name="username" id="username" class="form-control" placeholder="Username">
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label>รหัสผ่าน:<span>* (อย่างน้อย 6 ตัวอักษร)</span></label>
+										<input type="password" name="password" id="password" class="form-control" placeholder="Password">
+									</div>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label>ยืนยันรหัสผ่าน:<span>*</span></label>
+										<input type="password" name="confirm_password" id="confirm_password" class="form-control" placeholder="Confirm Password" >
+									</div>
 								</div>
 							</div>
-							<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
-								<div class="form-group">
-									<label for="confirmpwd">Confirm Password:</label>
-									{!! Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' => 'form-control')) !!}
+							<div class="row">
+								<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label for="usergroup">กลุ่มผู้ใช้:<span>*</span></label>
+										<select name="usergroup" id="usergroup" class="form-control selectpicker show-tick">
+											@role('root')
+											<option value="">-- เลือกกลุ่ม --</option>
+											<option value="-1">ผู้ดูแลระบบ</option>
+											@endrole
+											@foreach ($user_group as $key => $val)
+												<option value="{{ $key }}">{{ $val }}</option>
+											@endforeach
+										</select>
+									</div>
+								</div>
+								@can('user-create')
+								<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+									<div class="form-group">
+										<label>สิทธิ์ผู้ใช้:<span>*</span></label>
+										<select name="permission" id="permission" class="form-control selectpicker show-tick">
+											@role('root')
+												<option value="">-- โปรดเลือก --</option>
+												@foreach ($permissions as $key => $permission)
+													<option value="{{ $permission->name }}">{{ $permission->name }}</option>
+												@endforeach
+											@endrole
+											@role('ddc|ppc|pho|hos|lab')
+												<option value="11">user-create</option>
+											@endrole
+										</select>
+									</div>
+								</div>
+								@endcan
+							</div>
+							<div class="row">
+								<div class="col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 m-t-40">
+									<div class="form-group">
+										<input type="submit" value="บันทึกข้อมูล" class="btn btn-primary">
+									</div>
 								</div>
 							</div>
-							<div class="col-xs-12 col-sm-12 col-md-12">
-								<div class="form-group">
-									<label for="role">Role:</label>
-									{!! Form::select('roles[]', $roles,$userRole, array('class' => 'form-control')) !!}
-								</div>
-							</div>
-							<div class="col-xs-12 col-sm-12 col-md-12">
-								<button type="submit" class="btn btn-primary">Save</button>
-							</div>
-						</div>
-						{!! Form::close() !!}
+						</form>
 					</div>
 				</div>
 			</div>
@@ -95,10 +209,73 @@
 	</div>
 </div>
 @endsection
-@section('script')
+@section('bottom-script')
+<script src="{{ URL::asset('assets/libs/bootstrap-select-1.13.9/dist/js/bootstrap-select.min.js') }}"></script>
 <script>
 $(document).ready(function() {
-	$('.roles').select2();
+	$.ajaxSetup({
+		headers: {
+			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+		}
+	});
+	$('#prov_code').change(function() {
+		if ($(this).val() != "0") {
+			var id = $(this).val();
+			var idx = $(this).val();
+			$.ajax({
+				method: "POST",
+				url: "{{ route('render.district') }}",
+				dataType: "HTML",
+				data: {id:id},
+				success: function(response) {
+					$('#ampur_code').html(response);
+					$('#ampur_code').selectpicker("refresh");
+					$('#tambol_code').html('<option value="">-- โปรดเลือก --</option>');
+					$('#tambol_code').selectpicker("refresh");
+					$.ajax({
+						method: "POST",
+						url: "{{ route('render.hosp') }}",
+						dataType: "HTML",
+						data: {idx:idx},
+						success: function(res) {
+							$('#hospcode').html(res);
+							$('#hospcode').selectpicker("refresh");
+						},
+						error: function(xhr, status, error){
+							alert('Error code: ' + xhr.status + error);
+						}
+					});
+				},
+				error: function(jqXhr, textStatus, errorMessage){
+					alert('Error code: ' + jqXhr.status + errorMessage);
+				}
+			});
+
+		} else {
+			$('#ampur_code').html('<option value="">-- โปรดเลือก --</option>');
+			$('#tambol_code').html('<option value="">-- โปรดเลือก --</option>');
+			$('#hospcode').html('<option value="">-- โปรดเลือก --</option>');
+		}
+	});
+	$('#ampur_code').change(function() {
+		if ($(this).val() != "0") {
+			var id = $(this).val();
+			$.ajax({
+				method: "POST",
+				url: "{{ route('render.sub.district') }}",
+				dataType: "HTML",
+				data: {id:id},
+				success: function(response) {
+					$('#tambol_code').html(response);
+					$('#tambol_code').selectpicker("refresh");
+				},
+				error: function(jqXhr, textStatus, errorMessage){
+					alert('Error code: ' + jqXhr.status + errorMessage);
+				}
+			});
+		} else {
+			$('#tambol_code').html('<option value="">-- โปรดเลือก --</option>');
+		}
+	});
 });
 </script>
-@endsection
