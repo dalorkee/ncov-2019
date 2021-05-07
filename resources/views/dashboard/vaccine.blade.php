15cae084f8d75888c0c1c7f91443b9dd635d49c3
sataphat denduang <sataphat.dev@gmail.com>
2021-05-07 13:59:55 +0700

    view vaccine

---
+++ resources/views/dashboard/vaccine.blade.php
@@ -0,0 +1,16 @@
+<iframe
+  src="http://203.157.41.186/views/Covid-19/Covid-19?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link&:isGuestRedirectFromVizportal=y&:embed=y"
+  style="
+    position: fixed;
+    top: 0px;
+    bottom: 0px;
+    right: 0px;
+    width: 100%;
+    border: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    z-index: 999999;
+    height: 100%;
+  ">
+</iframe>
